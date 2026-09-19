703b06c79b42af519f61688927a204dab0154478
mbhadriraju <151967712+mbhadriraju@users.noreply.github.com>
2026-09-18 20:19:46 -0500

    center doc

--- app/doc/page.tsx
+++ app/doc/page.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const EmbeddedDoc = () => {
-  const docUrl = "https://docs.google.com/document/d/1ruxbSwe8Gn5MTcTxFNaYZS6QRofrC8k2awpDd9kgufU/edit?usp=sharing";
+  const docUrl =
+    "https://docs.google.com/document/d/1ruxbSwe8Gn5MTcTxFNaYZS6QRofrC8k2awpDd9kgufU/edit?usp=sharing";
 
   return (
-    <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
+    <div className="w-[80%] h-[1000px] border-2 border-gray-300 rounded-xl overflow-hidden shadow-sm mx-auto">
       <iframe
         src={docUrl}
         width="100%"
         height="100%"
         title="Google Doc Embed"
-        style={{ border: 'none' }}
+        style={{ border: "none" }}
         allowFullScreen
       />
     </div>
